37e45ef49224ac22f0a146dc3ff8aa9c3362a6f4
孙奥 <sunao@staff.tixa.com>
2025-01-20 17:07:29 +0800

    1212

---
+++ IOSNote/AObject.h
@@ -0,0 +1,16 @@
+//
+//  AObject.h
+//  IOSNote
+//
+//  Created by PF on 2025/1/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ IOSNote/AObject.m
@@ -0,0 +1,12 @@
+//
+//  AObject.m
+//  IOSNote
+//
+//  Created by PF on 2025/1/20.
+//
+
+#import "AObject.h"
+
+@implementation AObject
+
+@end
